e7b58274b2ccd7729feb25f98cc3178d6d4333ad
dongmin Goo <jokuk88@naver.com>
2024-11-04 03:28:04 +0900

    commit message

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
-const Home = () => {
-  return <div>Home</div>;
+
+const Home: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleLoginPage = () => {
+    navigate('/login');
+  };
+
+  return ( <div>
+    <h1>Home Page</h1>
+    <button onClick={handleLoginPage}>Go to Login</button>
+    </div> );
 };
 
 export default Home;
--- src/pages/SignUpPage/index.tsx
+++ src/pages/SignUpPage/index.tsx
@@ -1,7 +1,102 @@
-import React from "react";
+import React, {useState} from "react";
+import { useNavigate } from 'react-router-dom';
+import style from './SignUp.module.css';
 
-const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+interface SignupProps {
+  username: string;
+  emailOrphone: string;
+  password: string;
+  confirmpassword: string;
+}
+
+const SignUpPage: React.FC = () => {
+  const [form, setFrom] = useState<SignupProps> ({
+    username: '',
+    emailOrphone: '',
+    password: '',
+    confirmpassword: '',
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFrom({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (form.password !== form.confirmpassword) {
+      alert('비밀번호가 일치하지 않습니다!');
+      return;
+    }
+    console.log('Form Data:', form);
+    navigate('/login');
+  };
+
+  return (
+  <form className={style.form} onSubmit={handleSubmit}>
+    <div className={style.logodiv}>
+      <img src="/로고.png" alt="logo" className={style.logo}/>
+      <span className={style.logoText}>로그인</span>
+      </div>
+    <div className={style.div}>
+      <label className={style.label} htmlFor="username"></label>
+      <input
+        type="text"
+        id="username"
+        name="username"
+        value={form.username}
+        onChange={handleChange}
+        className={style.input}
+        placeholder="이름 또는 아이디"
+        required/>
+    </div>
+    <div className={style.div}>
+      <label className={style.label} htmlFor="emailOrPhone"></label>
+      <input
+        type="text"
+        id="emailOrphone"
+        name="emailOrphone"
+        value={form.emailOrphone}
+        onChange={handleChange}
+        className={style.input}
+        placeholder="이메일 또는 전화번호"
+        required/>
+    </div>
+    <div className={style.div}>
+      <label className={style.label} htmlFor="password"></label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        value={form.password}
+        onChange={handleChange}
+        className={style.input}
+        placeholder="비밀번호"
+        required/>
+    </div>
+    <div className={style.div}>
+      <label className={style.label} htmlFor="confirmPassword"></label>
+      <input
+        type="password"
+        id="confirmpassword"
+        name="confirmpassword"
+        value={form.confirmpassword}
+        onChange={handleChange}
+        className={style.input}
+        placeholder="비밀번호 확인"
+        required/>
+    </div>
+    <button className={style.button} type="submit">회원가입 </button> 
+    <div className={style.gologin}>
+      <span className={style.span}>이미 계정이 있으신가요?</span>
+      <p className={style.loginLink} onClick={() => navigate('/login')}>로그인 하러 가기</p>
+    </div>
+    </form>
+    );
 };
 
 export default SignUpPage;
