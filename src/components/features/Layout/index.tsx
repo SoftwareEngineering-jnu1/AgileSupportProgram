4212b7256f4347db7cae891d91bb8114cacf6d7f
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-11-03 16:29:46 +0900

    design: 페이지 너비 조절

--- src/components/features/Layout/index.tsx
+++ src/components/features/Layout/index.tsx
@@ -2,12 +2,16 @@ import styled from "styled-components";
 import { Outlet } from "react-router-dom";
 
 import Header, { HEADER_HEIGHT } from "../Header";
+import Menubar from "../Menubar";
 
 export const Layout = () => (
   <Wrapper>
     <Header />
     <InnerWrapper>
-      <Outlet />
+      <Menubar />
+      <Content>
+        <Outlet />
+      </Content>
     </InnerWrapper>
   </Wrapper>
 );
@@ -16,13 +20,20 @@ export default Layout;
 
 const Wrapper = styled.div`
   width: 100%;
+  height: 100%;
   position: relative;
 `;
 
 const InnerWrapper = styled.div`
   width: 100%;
+  height: 100%;
+  display: flex;
   position: absolute;
   top: 0;
   left: 0;
   margin-top: ${HEADER_HEIGHT};
+`;
+
+const Content = styled.div`
+  width: calc(100vw - 300px);
 `;
