a2dc5091f1abc8fa31f54f04d2e4228b01951bdd
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-11-28 02:37:39 +0900

    chore: 임시 데이터 추가

---
+++ src/components/Board/data.ts
@@ -0,0 +1,47 @@
+export const boardData = {
+  projectName: "testproject1",
+  sprintName: "testsprint2",
+  sprintEndData: "2024-12-23",
+  kanbanboardIssueDTO: [
+    {
+      issueId: 1,
+      issuetitle: "testissue1",
+      mainMemberNameAndcolor: {
+        강호정: "#B31460",
+      },
+      progressStatus: "Done",
+    },
+    {
+      issueId: 2,
+      issuetitle: "testissue2",
+      mainMemberNameAndcolor: {
+        기은빈: "#42C25E",
+      },
+      progressStatus: "In Progress",
+    },
+    {
+      issueId: 3,
+      issuetitle: "testissue3",
+      mainMemberNameAndcolor: {
+        강호정: "#B31460",
+      },
+      progressStatus: "To Do",
+    },
+    {
+      issueId: 4,
+      issuetitle: "testissue4",
+      mainMemberNameAndcolor: {
+        김봄: "#002260",
+      },
+      progressStatus: "To Do",
+    },
+    {
+      issueId: 5,
+      issuetitle: "testissue5",
+      mainMemberNameAndcolor: {
+        김봄: "#002260",
+      },
+      progressStatus: "Hold",
+    },
+  ],
+};
