f5350cc25fbe4bdc084f0d48ebaf666b2136c48f
dongmin Goo <jokuk88@naver.com>
2024-11-29 04:39:36 +0900

    feat: 메모페이지 메모 정렬, 삭제 기능 구현

--- src/pages/MemoPage/index.tsx
+++ src/pages/MemoPage/index.tsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 import style from './Memo.module.css';
+import {IoIosAdd} from "react-icons/io"; /*메모 추가 아이콘*/
+import { BsSortDown } from "react-icons/bs"; /*메모 정렬 아이콘*/
+import { IoIosArrowRoundUp } from "react-icons/io"; /*up 정렬 아이콘*/
+import { IoIosArrowRoundDown } from "react-icons/io"; /*down 정렬 아이콘*/
 
 interface ModalData {
   id: number;
   title: string;
   content: string;
+  timestamp: number;
 }
 
 const MemoPage: React.FC = () => {
@@ -13,6 +18,7 @@ const MemoPage: React.FC = () => {
   const [currentNote, setCurrentNote] = useState<ModalData | null>(null);
   const [noteTitle, setNoteTitle] = useState('');
   const [modalContent, setModalContent] = useState('');
+  const [sortedNewst, setsortedNewst] = useState(true);
   
   const handleOpenModal = () => {
     setIsModalOpen(true);
@@ -34,13 +40,18 @@ const MemoPage: React.FC = () => {
         id: Date.now(),
         title: noteTitle,
         content: modalContent,
+        timestamp: Date.now(),
       };
-      setSaveModals([...savedModals, newModal]);
+      setSaveModals([newModal, ...savedModals]);
       setIsModalOpen(false);
       }
       handleCloseModal();
   };
-
+  const deletememo = (id: number) => {
+    if(window.confirm('정말로 이 메모를 삭제하시겠습니까?')) {
+      setSaveModals(savedModals.filter(modal => modal.id !== id));
+    }
+  };
   const editNote = (modal: ModalData) => {
     setCurrentNote(modal);
     setNoteTitle(modal.title);
@@ -48,9 +59,31 @@ const MemoPage: React.FC = () => {
     handleOpenModal();
   };
 
+  
+
+  const sortmemo = () => {
+    const sorted = [...savedModals].sort((a,b) => sortedNewst ?  a.timestamp - b.timestamp : b.timestamp - a.timestamp);
+    setSaveModals(sorted);
+    setsortedNewst(!sortedNewst);
+  };
+
+  const formatDate = (timestamp: number) => {
+    const date = new Date(timestamp);
+    return `${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')} 
+    ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`
+  }
+
   return (
     <div className={style.memopagediv}>
-      <button onClick={handleOpenModal}>메모 생성</button>
+      <button className={style.memoadd} onClick={handleOpenModal}>
+        <IoIosAdd className={style.addicon}/>
+      </button>
+      <div className={style.sortdiv}> 
+        <BsSortDown className={style.memosorticon}/><p> 만든 날짜 순</p><p className={style.p2}>|</p>
+        <p className={style.memosort} onClick={sortmemo}>
+        {sortedNewst ? <IoIosArrowRoundDown className={style.sorticon}/> : <IoIosArrowRoundUp className={style.sorticon}/>}</p>
+      </div>
+      
 
       {isModalOpen && (
         <div className={style.overlay}>
@@ -60,14 +93,15 @@ const MemoPage: React.FC = () => {
               type="text"
               value={noteTitle}
               onChange={(e) => setNoteTitle(e.target.value)}
-              placeholder="제목을 입력해주세요"
+              placeholder="제목"
             />
-            <input className={style.textarea}
+            <textarea className={style.textarea}
               value={modalContent}
               onChange={(e) => setModalContent(e.target.value)}
               placeholder="내용"
-              ></input>
-            <button className={style.savebutton} onClick={handleSaveModal}>Save</button>
+              ></textarea>
+            <button className={style.savebutton} onClick={handleSaveModal}>저장</button>
+            
           </div>
         </div>
       )}
@@ -75,8 +109,10 @@ const MemoPage: React.FC = () => {
       <div className={style.savemodal}>
         {savedModals.map((modal) => (
           <div key={modal.id} className={style.modalitem} onClick={() => editNote(modal)}>
+            <h3>생성날짜: {formatDate(modal.timestamp)}</h3>
             <h3>제목: {modal.title}</h3>
-            <p>{modal.content}</p>
+            <p className={style.memocontent}>{modal.content}</p>
+            <span className={style.deletememobutton} onClick={() => deletememo(modal.id)}>&times;</span>
           </div>
         )
       )}
